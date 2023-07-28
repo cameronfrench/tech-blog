edb983460b940131b3e48c4f356256eb23843422
Cameron French <cameron.french92@gmail.com>
2023-07-28 15:57:29 -0400

    setup all initial files including config, controllers, db, models, public, seeds, views, env, gitignore, and server

---
+++ seeds/blogData.js
@@ -0,0 +1,15 @@
+const { Blogpost } = require('../models');
+
+const blogdata = [
+  {
+    title: 'MVC',
+    author: 'Cameron French',
+    createdOn: 'June 21, 2021 17:00:00',
+    content: 'This is content',
+
+  },
+];
+
+const seedBlog = () => Blogpost.bulkCreate(blogdata);
+
+module.exports = seedBlog;
