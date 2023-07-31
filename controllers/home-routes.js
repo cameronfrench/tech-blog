15ac893252d686937a80a4eb25aa9d1735a01ce1
Cameron French <cameron.french92@gmail.com>
2023-07-30 23:38:03 -0400

    added login and logout under js file under public. Also added api folder in controllers and setup user routes

---
+++ controllers/home-routes.js
@@ -0,0 +1,63 @@
+const router = require('express').Router();
+const { Blogpost } = require('../models');
+
+// GET all blogs for homepage
+router.get('/', async (req, res) => {
+  try {
+    const dbBlogpostData = await Blogpost.findAll({
+      include: [
+        {
+          model: Blogpost,
+          attributes: ['title', 'author', 'createdOn'],
+        },
+      ],
+    });
+
+    const blogposts = dbBlogpostData.map((blogpost) =>
+      gallery.get({ plain: true })
+    );
+    res.render('homepage', {
+      blogposts,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// GET one blog
+router.get('/blog/:id', async (req, res) => {
+  try {
+    const dbBlogpostData = await Blogpost.findByPk(req.params.id, {
+      include: [
+        {
+          model: Blogpost,
+          attributes: [
+            'title',
+            'author',
+            'createdOn',
+            'content',
+          ],
+        },
+      ],
+    });
+
+    const blogpost = dbBlogpostData.get({ plain: true });
+    res.render('blogpost', { blogpost, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// Login route
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('login');
+});
+
+module.exports = router;
--- seeds/blogData.js
+++ seeds/blogData.js
@@ -8,6 +8,20 @@ const blogdata = [
     content: 'This is content',
 
   },
+  {
+    title: 'ORM',
+    author: 'Cameron French',
+    createdOn: 'June 21, 2021 17:00:00',
+    content: 'This is content',
+
+  },
+  {
+    title: 'MySQL',
+    author: 'Cameron French',
+    createdOn: 'June 21, 2021 17:00:00',
+    content: 'This is content',
+
+  },
 ];
 
 const seedBlog = () => Blogpost.bulkCreate(blogdata);
